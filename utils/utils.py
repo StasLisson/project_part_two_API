6bc6ee6afd4e92872344019dce624a85336e52b8
staslisson0707 <staslisson0707@gmail.com>
2025-12-10 22:53:34 +0200

    added faker
    added faker

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
 import random
 import string
+from faker import Faker
 
+fake = Faker()
 
 class Utils:
     @staticmethod
@@ -8,10 +10,24 @@ class Utils:
         return {key: value for key, value in data_dictionary.items() if value is not None}
 
     @staticmethod
+    def generate_email():
+        return fake.unique.email()
+
+    @staticmethod
+    def generate_password():
+        return fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
+
+    @staticmethod
+    def generate_first_name():
+        return fake.first_name()
+
+    @staticmethod
+    def generate_last_name():
+        return fake.last_name()
+
+    @staticmethod
     def generate_random_string(length=10, include_special=False):
         chars = string.ascii_letters + string.digits
-
         if include_special:
             chars += string.punctuation + " "
-
         return ''.join(random.choice(chars) for _ in range(length))
